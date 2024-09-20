942a91d51e35554c065c4d6fe7daaa60a18357b3
Prashant2303 <prashant1997.official@gmail.com>
2024-09-20 17:46:57 +0530

    Integrated Google Gemini for Suggestions Feature

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import user from './users/userById.js';
 import lists from './lists/index.js';
 import listById from './lists/listById.js';
 import questions from './questions/index.js';
+import suggestions from './lists/suggestions.js';
+import { authCheck } from './helpers/api-handler.js';
 
 const router = express.Router();
 
@@ -22,5 +24,7 @@ router.post('/lists', lists);
 router.post('/questions/*', questions);
 router.patch('/questions/*/*', questions);
 router.delete('/questions/*/*', questions);
+
+router.get('/suggestions/:listId', authCheck, suggestions);
 
 export default router;
---
+++ api/lists/suggestions.js
@@ -0,0 +1,42 @@
+import { apiHandler } from '../helpers/api-handler.js';
+import { ObjectId } from 'mongodb';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const key = process.env.GEMINI_KEY;
+const genAI = new GoogleGenerativeAI(key);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash", generationConfig: {
+        responseMimeType: "application/json"
+    },
+});
+
+export default apiHandler({ get });
+
+async function get({ req, res, listsCollection }) {
+    const { listId } = req.params;
+
+    const result = await listsCollection.findOne({ _id: new ObjectId(listId) }, { projection: { questions: 1, _id: 0 } });
+
+    const questions = result?.questions;
+
+    let prompt = '';
+    if (questions.length === 0) {
+        prompt = 'Suggest me 10 beginner questions to start learning Data Structures and Algorithms.'
+    } else {
+        prompt = "Suggest me new questions based on the topics covered in the following questions and respond using this json JSON schema Question = {'questionName': string} Topic = {'topicName': string, 'questionList': Array<Question>} Return: Array<Topic>`;"
+
+        result?.questions.forEach((question, index) => {
+            prompt += ` ${index + 1}. ${question.name}`
+        });
+    }
+
+    try {
+        const geminiResult = await model.generateContent(prompt);
+        const data = geminiResult.response.text();
+        return res.status(200).json(JSON.parse(data));
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Server Error');
+    }
+}
