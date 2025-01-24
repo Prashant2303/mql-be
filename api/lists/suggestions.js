f1cd564df4a750f8b1e064fd2aa544710fa8a033
Prashant2303 <prashant1997.official@gmail.com>
2025-01-24 19:58:46 +0530

    Updated gemini prompt and fixed response edge case

--- api/lists/suggestions.js
+++ api/lists/suggestions.js
@@ -5,7 +5,8 @@ const key = process.env.GEMINI_KEY;
 const genAI = new GoogleGenerativeAI(key);
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash", generationConfig: {
+    model: "gemini-1.5-flash",
+    generationConfig: {
         responseMimeType: "application/json"
     },
 });
@@ -21,7 +22,7 @@ export async function getSuggestions(req, res) {
     if (questions.length === 0) {
         prompt = 'Suggest me 10 beginner questions to start learning Data Structures and Algorithms.'
     } else {
-        prompt = "Suggest me new questions based on the topics covered in the following questions and respond using this json JSON schema Question = {'questionName': string} Topic = {'topicName': string, 'questionList': Array<Question>} Return: Array<Topic>`;"
+        prompt = "Suggest me new questions based on the topics covered in the following questions. Each topic should contain five questions and respond using this json JSON schema: Question = {'questionName': string} Topic = {'topicName': string, 'questionList': Array<Question>} Return: Array<Topic>";
 
         result?.questions.forEach((question, index) => {
             prompt += ` ${index + 1}. ${question.name}`
@@ -30,8 +31,12 @@ export async function getSuggestions(req, res) {
 
     try {
         const geminiResult = await model.generateContent(prompt);
-        const data = geminiResult.response.text();
-        return res.status(200).json(JSON.parse(data));
+        const response = geminiResult.response.text();
+        let data = JSON.parse(response);
+        if (!Array.isArray(data)) {
+            data = data[Object.keys(data)[0]];
+        }
+        return res.status(200).send(data);
     } catch (error) {
         console.log(error);
         return res.status(500).send('Server Error');
